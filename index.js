e346230601fbe3ad5c2c74ac6b6a9c0f6cca3070
fanboykun <irfanramadhan1812@gmail.com>
2024-01-25 15:08:38 +0700

    change json array to json object

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    let a = []
+    // let a = []
     const obj1 = {
         type: 0,
         content: 'My Title',
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
         format: 3
     };
     a.push(obj1)
-  res.json(a);
+  res.json(obj1);
+  // res.json(a);
 });
 
 app.listen(3000, () => {
