ac599225afbd1653caf817dadad89de2f9b0cfe5
fanboykun <irfanramadhan1812@gmail.com>
2024-01-25 14:55:48 +0700

    add server handler

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    let a = []
+    const obj1 = {
+        type: 0,
+        content: 'My Title',
+        bold: 1, 
+        align: 2,
+        format: 3
+    };
+    a.push(obj1)
+  res.json(a);
+});
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
